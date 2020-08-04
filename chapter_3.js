2772d1dc12122f228e8f04670e82ee960a0575d3
hisa008 <hisayuki.k8@gmail.com>
2020-08-04 20:03:05 +0900

    [finish]chapter3_22

--- chapter_3.js
+++ chapter_3.js
@@ -1,3 +1,26 @@
-var str = "WINGSプロジェクト";
-window.alert(str.substring(5,-2));
-window.alert(str.slice(5,-2));
+// var str = "WINGSプロジェクト";
+// window.alert(str.substring(5,-2));
+// window.alert(str.slice(5,-2));
+
+// window.alert(Number.MAX_SAFE_INTEGER + 2);
+
+// var data = [2, 3, 4, 5];
+// data.forEach(function(value, index, array){
+//     window.alert(array * array);
+// });
+// var result = data.map(function(value, index, array){
+//     return index * index;
+// });
+
+// window.alert(result);
+
+var data = [4, 9, 16, 25];
+var result = data.some(function(value, index, array){
+    return value % 2 === 0;
+});
+
+if(result){
+    window.alert('3の倍数');
+}else{
+    window.alert('3の倍数ではない');
+}

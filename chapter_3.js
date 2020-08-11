fc22ef0206243478db5cba67140795172ba48172
hisa008 <hisayuki.k8@gmail.com>
2020-08-11 13:35:25 +0900

    [updata]chapter_3

--- chapter_3.js
+++ chapter_3.js
@@ -14,13 +14,36 @@
 
 // window.alert(result);
 
-var data = [4, 9, 16, 25];
-var result = data.some(function(value, index, array){
-    return value % 2 === 0;
-});
+// var data = [4, 9, 16, 25];
+// var result = data.some(function(value, index, array){
+//     return value % 2 === 0;
+// });
 
-if(result){
-    window.alert('3の倍数');
-}else{
-    window.alert('3の倍数ではない');
+// if(result){
+//     window.alert('3の倍数');
+// }else{
+//     window.alert('3の倍数ではない');
+// }
+
+// var ary = [5, 25, 10];
+// window.alert(ary.sort());
+// window,alert(ary.sort(function(x, y){
+//     return y - x;
+// }));
+
+let m = new Map();
+m.set('dog', 'ワンワン');
+m.set('cat', 'ニャー');
+m.set('mouse', 'チュー');
+
+// for (let key of m.keys()){
+//     window.alert(key);
+// }
+
+// for (let value of m.values()){
+//     window.alert(value);
+// }
+
+for (let [key, value] of m){
+    window.alert(key);
 }

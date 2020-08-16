0aa3c70343e2fd47d01752f022f416398feda453
hisa008 <hisayuki.k8@gmail.com>
2020-08-16 16:45:06 +0900

    [finish]chapter3_7

--- chapter3_7.js
+++ chapter3_7.js
@@ -1,2 +1,10 @@
-var p = 'start';
-var rank = 'A';
+// var str = 'console.log("eval関数")';
+// eval(str);
+
+// var obj = {hoge: 1, foo: 2};
+// var prop = 'hoge';
+// eval('console.log(obj.'+ prop + ')');
+// console.log(obj[prop]);
+
+eval('var data = {"hoge": 1, "foo": 2}');
+console.log(data.hoge);

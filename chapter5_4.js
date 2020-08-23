5b369b68e5baff3188e7b89e8a8cc409c7d7b605
hisa008 <hisayuki.k8@gmail.com>
2020-08-23 23:09:29 +0900

    [updata]chapter5_4

--- chapter5_4.js
+++ chapter5_4.js
@@ -1,4 +1,93 @@
+// function Triangle(){
+//     var _base;
+//     var _height;
+
+
+//     var _checkArgs = function(val){
+//         return(typeof val === 'number' && val > 0);
+//     }
+
+//     this.setBase = function(base){
+//         if(_checkArgs(base)){
+//             _base = base;
+//         }
+//     }
+//     this.getBase = function(){
+//         return _base;
+//     }
+
+//     this.setHeight = function(height){
+//         if(_checkArgs(height)){
+//             _height = height;
+//         }
+//     }
+//     this.getHeight = function(){
+//         return _height;
+//     }
+// }
+
+// Triangle.prototype.getArea = function(){
+//     return this.getBase() * this.getHeight()/2;
+// }
+
+// var t = new Triangle();
+// t._base = 10;
+// t._height = 2;
+// console.log('三角形の面積:' + t.getArea());
+
+// t.setBase(10);
+// t.setHeight(2);
+// console.log('三角形の底辺:' + t.getBase());
+// console.log('三角形の高さ:' + t.getHeight());
+// console.log('三角形の面積:' + t.getArea());
+
+
+
+
+
 function Triangle(){
     var _base;
     var _height;
+
+    Object.defineProperty(
+        this,
+        'base',
+        {
+            get: function(){
+                return _base;
+            },
+            set: function(base){
+                if(typeof base === 'number' && base > 0){
+                    _base = base;
+                }
+            }
+        }
+    );
+
+    Object.defineProperty(
+        this,
+        'height',
+        {
+            get: function(){
+                return _height;
+            },
+            set: function(height){
+                if(typeof height){
+                    if(typeof height === 'number' && height > 0){
+                        _height = height;
+                    }
+                }
+            }
+        }
+    );
 }
+
+Triangle.prototype.getArea = function(){
+    return this.base * this.height/2;
+}
+
+var t = new Triangle();
+t.base = 10;
+t.height = 2;
+console.log('三角形の底辺:' + t.base);
+console.log('三角形の高さ:' + t.height);

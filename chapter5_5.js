ea9f4188076df171148f9d1f8e1bdec416fc572d
hisa008 <hisayuki.k8@gmail.com>
2020-08-26 15:45:30 +0900

    [start]chapter6_1

--- chapter5_5.js
+++ chapter5_5.js
@@ -18,33 +18,45 @@
 
 
 
-class Member{
-    constructor(firstName, lastName){
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+// class Member{
+//     constructor(firstName, lastName){
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//     }
 
-    get firstName(){
-        return this._firstName;
-    }  
+//     get firstName(){
+//         return this._firstName;
+//     }  
 
-    set firstName(value){
-        this._firstName = value;
-    }
+//     set firstName(value){
+//         this._firstName = value;
+//     }
 
-    get lastName(){
-        return this._lastName;
-    }
+//     get lastName(){
+//         return this._lastName;
+//     }
 
-    set lastName(value){
-        this._lastName = value;
-    }
+//     set lastName(value){
+//         this._lastName = value;
+//     }
 
-    getName(){
-        return this.lastName + this.firstName;
-    }
-}
+//     getName(){
+//         return this.lastName + this.firstName;
+//     }
+// }
 
-let m = new Member('太郎', '山田');
-console.log(m.getName());
-console.log(m.lastName());
+// let m = new Member('太郎', '山田');
+// console.log(m.getName());
+// console.log(m.lastName());
+
+
+
+
+
+// class Area{
+//     static getTriangle(base, height){
+//         return base * height/2;
+//     }
+// }
+
+// console.log(Area.getTriangle(10, 5));

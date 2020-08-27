300fb93c2611abadf138601f2093bae9e2da0f8c
hisa008 <hisayuki.k8@gmail.com>
2020-08-27 23:03:50 +0900

    [updata]chapter6_3

--- chapter6_3.js
+++ chapter6_3.js
@@ -1,3 +1,15 @@
+// document.addEventListener('DOMContentLoaded', function(){
+//     var logo = document.getElementById('logo');
+// }
+
+
+
+
 document.addEventListener('DOMContentLoaded', function(){
     var logo = document.getElementById('logo');
-}
+    var attrs = logo.attributes;
+    for(var i = 0, len = attrs.length; i < len; i++){
+        var attr = attrs.item(i);
+        console.log(attr.name + ':' + attr.value);
+    }
+}, false);

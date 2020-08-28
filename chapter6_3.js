02c22f0a380facdcb92121cb11f7e5ab40295876
hisa008 <hisayuki.k8@gmail.com>
2020-08-28 23:44:26 +0900

    [updata]chapter6_3

--- chapter6_3.js
+++ chapter6_3.js
@@ -5,11 +5,44 @@
 
 
 
+// document.addEventListener('DOMContentLoaded', function(){
+//     var logo = document.getElementById('logo');
+//     var attrs = logo.attributes;
+//     for(var i = 0, len = attrs.length; i < len; i++){
+//         var attr = attrs.item(i);
+//         console.log(attr.name + ':' + attr.value);
+//     }
+// }, false);
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function(){
+//     document.getElementById('result_text').textContent = 
+//         '<a href="http://www.wings.msn.to/">WINGSプロジェクト</a>';
+//     document.getElementById('result_html').innerHTML =
+//         '<a href="http://www.wings.msn.to/">WINGSプロジェクト</a>';
+// }, false);
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function(){
+//     var list = document.getElementById('list');
+//     console.log(list.innnerHTML);
+//     console.log(list.textContent);
+// }, false);
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', function(){
-    var logo = document.getElementById('logo');
-    var attrs = logo.attributes;
-    for(var i = 0, len = attrs.length; i < len; i++){
-        var attr = attrs.item(i);
-        console.log(attr.name + ':' + attr.value);
-    }
+    document.getElementById('btn').addEventListener('click', function(){
+        var name = document.getElementById('name');
+        var result = document.getElementById('result');
+        result.innerHTML = 'こんにちは、' + name.value + 'さん！';
+    }, false);
 }, false);
